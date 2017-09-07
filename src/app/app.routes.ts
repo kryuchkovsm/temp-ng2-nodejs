65cb043fbea901449267d56aa87e28c7fff67256
sergey_k <kryuchkovsm.exceed@gmail.com>
2017-09-07 12:33:59 +0300

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { ModuleWithProviders }         from '@angular/core';
+import { Routes, RouterModule }        from '@angular/router';
+
+import { AuthGuard }                   from './guards/auth.guard'
+import { WelcomePageComponent}         from './components/welcomepage.component/welcomepage.component';
+import { BoardComponent }              from './modules/board/components/board.component/board.component';
+import { LoginComponent }              from './components/login.component/login.component';
+import { SignUpComponent }             from './components/signup.component/index';
+import { DashBoardComponent}           from './components/dashboard.component/dashboard.component';
+import { UserProfileComponent }        from './components/userprofile.component/userprofile.component';
+
+const appRoutes: Routes = [
+    { path: '',             component: DashBoardComponent, canActivate: [AuthGuard] },
+    { path: 'dashboard',    component: DashBoardComponent, canActivate: [AuthGuard] },
+    { path: 'login',        component: LoginComponent },
+    { path: 'signup',       component: SignUpComponent },
+    { path: 'forbidden',    component: LoginComponent},
+    { path: 'welcome',      component: WelcomePageComponent },
+    { path: 'profile',      component: UserProfileComponent}];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
